25874e690a4fddfa895a1624b80fe9ce32a30b39
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2026-01-14 16:25:24 -0300

    Adicionar pages e Header

---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id excepturi voluptatibus a deleniti magnam necessitatibus est eveniet delectus obcaecati error. Sint fugiat illo quasi neque earum inventore expedita provident asperiores?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
